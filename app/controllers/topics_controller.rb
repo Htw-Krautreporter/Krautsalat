ed86948bba9da5ef498d84b7d27e23ae2d76eeef
jurialexander <s0547951@htw-berlin.de>
2016-05-31 16:25:55 +0200

    Topics actions index, show, edit, update

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,11 @@
 class TopicsController < ApplicationController
+	before_action :find_topic, only: [:show, :edit, :update, :destroy]
 
 	def index
+		@topics = Topic.all.order("created_at DESC")
+	end
+
+	def show
 	end
 
 	def new
@@ -8,13 +13,36 @@ class TopicsController < ApplicationController
 	end
 
 	def create
-		@topic = Topic(topic_params)	
+		@topic = Topic.new(topic_params)
+		if @topic.save
+			redirect_to topics_path
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @topic.update(topic_params)
+			redirect_to topic_path
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
 	end
 
 	private
 
 		def topic_params
 			params.require(:topic).permit(:title, :description)
+		end
+
+		def find_topic
+			@topic = Topic.find(params[:id])
 		end
 
 end
