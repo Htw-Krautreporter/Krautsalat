a957f3b5a334f60c2c6302856ed95e92b17a0945
TamaraVoigt <TamaraVoigt@online.de>
2016-06-02 16:06:23 +0200

    Added admin, created seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160602123556) do
+ActiveRecord::Schema.define(version: 20160602131414) do
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160602123556) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
