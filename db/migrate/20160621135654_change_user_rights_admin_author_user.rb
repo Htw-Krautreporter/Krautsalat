09559cc37ad2b2308099373ae9160cfdd41263bf
Enne <enne.ddn@gmail.com>
2016-06-21 16:14:49 +0200

    username is implemented in all needed ways

---
+++ db/migrate/20160621135654_change_user_rights_admin_author_user.rb
@@ -0,0 +1,6 @@
+class ChangeUserRightsAdminAuthorUser < ActiveRecord::Migration
+  def change
+  	#add_column :user, :rightlevel, :integer
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160602144617) do
+ActiveRecord::Schema.define(version: 20160621135654) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
