44b630eba2d2d065b6d868c60e2a9d51accb661a
Enne <enne.ddn@gmail.com>
2016-06-28 15:10:15 +0200

    Username uniqueness and time formatation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,7 @@ module ApplicationHelper
 		time = time.in_time_zone("Berlin")
 		@day = time.day < 10 ? "0#{time.day}" : time.day
 		@month = time.month < 10 ? "0#{time.month}" : time.month
+		@min = time.min < 10 ? "0#{time.min}" : time.min
 		"#{@day}.#{@month}.#{time.year} um #{time.hour}:#{time.min} Uhr"
 	end
 	
