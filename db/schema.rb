6901662d2c44938646e3e75397e05791bd18d91d
jurialexander <s0547951@htw-berlin.de>
2016-07-13 16:35:14 +0200

    Removed unecessary if statement

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621135654) do
+ActiveRecord::Schema.define(version: 20160705144525) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -43,24 +43,33 @@ ActiveRecord::Schema.define(version: 20160621135654) do
     t.integer  "user_id"
   end
 
+  create_table "topics_users", id: false, force: :cascade do |t|
+    t.integer "topic_id"
+    t.integer "user_id"
+  end
+
+  add_index "topics_users", ["topic_id"], name: "index_topics_users_on_topic_id"
+  add_index "topics_users", ["user_id"], name: "index_topics_users_on_user_id"
+
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "username",               default: "",    null: false
-    t.boolean  "admin",                  default: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
+    t.string   "username",               default: "", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
