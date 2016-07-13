634da46610c32d1cd8945f7fef8ef0cfbf5e7b93
jurialexander <s0547951@htw-berlin.de>
2016-07-13 16:00:34 +0200

    Fixed helper method signature_string and activators/ids of posts and comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,15 +2,15 @@ module ApplicationHelper
 
 	def time_string(time)
 		time = time.in_time_zone("Berlin")
-		@day = time.day < 10 ? "0#{time.day}" : time.day
-		@month = time.month < 10 ? "0#{time.month}" : time.month
-		"#{@day}.#{@month}.#{time.year} um #{time.hour}:#{time.min} Uhr"
+		day = time.day < 10 ? "0#{time.day}" : time.day
+		month = time.month < 10 ? "0#{time.month}" : time.month
+		"#{day}.#{month}.#{time.year} um #{time.hour}:#{time.min} Uhr"
 	end
 
 	def signature_string(author, created_at, updated_at)
-		@created = "Verfasst von #{author} am #{time_string(created_at)}. "
-		@updated = updated_at > created_at ? "Zuletzt bearbeitet am #{time_string(updated_at)}." : ""
-		@created + @updated
+		created = "Verfasst von #{author} am #{time_string(created_at)}. "
+		updated = created_at.to_s != updated_at.to_s ? "Zuletzt bearbeitet am #{time_string(updated_at)}." : ""
+		created + updated
 	end
 	
 end
