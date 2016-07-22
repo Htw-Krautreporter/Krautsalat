2886a087feccaeae41c06024c96fc41da94677ce
jurialexander <s0547951@htw-berlin.de>
2016-07-22 13:06:23 +0200

    Removed empty migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160705144525) do
+ActiveRecord::Schema.define(version: 20160719110153) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -52,20 +52,21 @@ ActiveRecord::Schema.define(version: 20160705144525) do
   add_index "topics_users", ["user_id"], name: "index_topics_users_on_user_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.boolean  "admin",                  default: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin"
-    t.string   "username",               default: "", null: false
+    t.string   "username",               default: "",    null: false
+    t.string   "expertise",              default: ""
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
