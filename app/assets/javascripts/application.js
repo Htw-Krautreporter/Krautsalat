b2f77cf65bd91fac50018b62dbeea550fbf93631
Julia <b.prudence@gmx.de>
2016-07-24 11:56:29 +0200

    struggeling with JavaScript, trying to add event listener on ok/cancel button for inline edit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,77 @@
 function disableButton(id) {
 	document.getElementById(id).disabled = true;
 }
+
+function enableButton(id) {
+	document.getElementById(id).disabled = false;
+}
+
+function enableButtons() {
+	var disabledButtons = getElementsByAttribute('disabled');
+	for(var buttonId = 0; buttonId < disabledButtons.length; buttonId ++) {
+		disabledButtons(buttonId).disabled = false;
+	}
+}
+
+function wrapButtons() {
+	console.log("wrap");
+	var save = document.getElementsByClassName("button-save");
+	var cancel = document.getElementsByClassName("button-cancel");
+	console.log(save.length);
+	for(var i = 0; i < save.length; i++) {
+		//console.log(i);
+		var parent = save[i].parent;
+		var wrapper = document.createElement('div').classList.add('btn-edit');
+		parent.appendChild(wrapper);
+		wrapper.appendChild(save[i]);
+		wrapper.appendChild(cancel[i]);
+	}
+}
+
+function topicOnClick(id){
+	if(id != -1){
+		disableButton(id);
+	}
+
+	//wrapButtons();
+}
+
+function getElementsByAttribute(attr) {
+    // get all dom elements
+    var elements = document.getElementsByTagName("*");
+    // initialize array to put matching elements into
+    var foundelements = [];
+
+    // loop through all elements in document
+    for (var elementId = 0; elementId < elements.length; elementId++) {
+        // check to see if element has any attributes
+        if (elements[elementId].attributes.length > 0) {
+            // loop through element's attributes  and add it to array if it matches attribute from argument
+            for (var attrId = 0; attrId < elements[elementId].attributes.length; attrId++) {
+                if (elements[elementId].attributes[attrId].name === attr) {
+                    foundelements.push(elements[elementId]);
+                }
+            }
+        }
+    }
+    return foundelements;
+}
+
+/*window.onload = function() {
+	var buttons = document.getElementsByClassName('button-cancel');
+	console.log('This kinda works: ' + buttons.length);
+	for (var buttonId = 0; buttonId < buttons.length; buttonId ++) {
+		console.log('Added Listener for: ' + buttonId);
+		buttons[buttonId].addEventListener('click', function() { alert('test') }, false);
+	}
+};
+document.addEventListener("DOMSubtreeModified", function(e) {
+	var buttons = document.getElementsByClassName('button-cancel');
+	console.log('This kinda works: ' + buttons.length);
+
+	for (var buttonId = 0; buttonId < buttons.length; buttonId ++) {
+		console.log('Added Listener for: ' + buttonId);
+		buttons[buttonId].addEventListener('click', function() { alert('test') }, false);
+	}
+	
+}, false);*/
