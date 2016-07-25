58302eeebd864dff71d3e361ca6280fcb5238c2d
Julia <b.prudence@gmx.de>
2016-07-25 14:54:27 +0200

    add images, and links to posts and comments

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,38 @@
 //= require_tree .
 
 
+function appendImage(id) {
+	var imageUrl = prompt("Fügen Sie hier die URL zu einem Bild ein.", "http://...");
+	if(imageUrl == null)
+	var imageTag = "";
+	else
+	var imageTag = "<img src=\"" + validateUrl(linkUrl) + "\" class=\"content-images\" \\>";
+	document.getElementById(id).value += imageTag;
+}
+
+function appendLink(id) {
+	var linkUrl = prompt("Fügen Sie hier eine URL ein.", "http://...");
+	var linkText = prompt("Fügen Sie hier einen Linktext ein. (optional)");
+	if(linkUrl == null) 
+	var link = "";
+	else {
+		if (linkText == null)
+			linkText = linkUrl;
+		var link = "<a href=\"" + validateUrl(linkUrl) + "\" class=\"content-link\" target=\"_blank\" \\>" + linkText + "</a>";
+	}
+	document.getElementById(id).value += link;
+}
+
+function appendBoldText() {
+	
+}
+
+function validateUrl(url) {
+	var pattern = /^((http|https|ftp):\/\/)/;
+
+	if(!pattern.test(url)) 
+	    return "http://" + url;
+	else
+		return url
+}
+
