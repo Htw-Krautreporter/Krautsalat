27c25a234973956f680fd725bda48eaf5470f4f7
Enne <enne.ddn@gmail.com>
2016-07-26 17:56:34 +0200

    validation username complete with messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ActiveRecord::Base
 	devise :database_authenticatable, :registerable,
 		:recoverable, :rememberable, :trackable, :validatable
 
-	validates :username, uniqueness: true
+	validates_uniqueness_of :username, message: "Der Username ist bereits vergeben."
+	validates_length_of :username, minimum: 3, too_short: "Der Username muss mindestens drei Zeichen enthalten."
 
 	def self.search(search)
 		where("email LIKE ? OR username LIKE ? OR expertise LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
